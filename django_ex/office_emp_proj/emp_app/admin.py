33a9bcffe0bccf702acdcab240bfa40cb01ad984
Sai-Krishna-G <117513682+Sai-Krishna-G@users.noreply.github.com>
2023-02-07 18:54:43 +0530

    Add files via upload

---
+++ django_ex/office_emp_proj/emp_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Employee,role,Department
+
+
+# Register your models here.
+admin.site.register(Employee)
+admin.site.register(role)
+admin.site.register(Department)
